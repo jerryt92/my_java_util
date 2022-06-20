e4273442b550535516ba84c8db775b15af7625bc
tianjingli <569302975@qq.com>
2022-06-20 14:16:09 +0800

    移除了无用的内容

--- src/main/java/com/tjl/sample/UUID/Main.java
+++
@@ -1,9 +0,0 @@
-package com.tjl.sample.UUID;
-
-import java.util.UUID;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(UUID.randomUUID().toString());
-    }
-}
